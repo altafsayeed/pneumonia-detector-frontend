faff811e444974967091f64cb51255f6579001ed
altafsayeed <altafsayeed19@yahoo.com>
2022-11-07 02:36:36 -0800

    UI tweaks

--- src/views/IndexView/components/Hero/Hero.js
+++ src/views/IndexView/components/Hero/Hero.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { useMediaQuery, Grid, Button, Typography } from '@material-ui/core';
-import { Image } from '../../../../components/atoms';
-import { SectionHeader } from '../../../../components/molecules';
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useMediaQuery, Grid, Button, Typography } from "@material-ui/core";
+import { Image } from "../../../../components/atoms";
+import { SectionHeader } from "../../../../components/molecules";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   image: {
     boxShadow:
-      '25px 60px 125px -25px rgba(80,102,144,.1), 16px 40px 75px -40px rgba(0,0,0,.2)',
+      "25px 60px 125px -25px rgba(80,102,144,.1), 16px 40px 75px -40px rgba(0,0,0,.2)",
     borderRadius: theme.spacing(2),
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       maxWidth: 500,
     },
   },
 }));
 
-const Hero = props => {
+const Hero = (props) => {
   const { className, ...rest } = props;
   const classes = useStyles();
 
   const theme = useTheme();
-  const isMd = useMediaQuery(theme.breakpoints.up('md'), {
+  const isMd = useMediaQuery(theme.breakpoints.up("md"), {
     defaultMatches: true,
   });
 
@@ -32,7 +31,7 @@ const Hero = props => {
         container
         justify="space-between"
         spacing={4}
-        direction={isMd ? 'row' : 'column-reverse'}
+        direction={isMd ? "row" : "column-reverse"}
       >
         <Grid
           item
@@ -40,12 +39,12 @@ const Hero = props => {
           alignItems="center"
           xs={12}
           md={6}
-          data-aos={'fade-up'}
+          data-aos={"fade-up"}
         >
           <SectionHeader
             title={
               <span>
-                Use cutting edge Machine Learning
+                Use cutting edge Machine Learning technology
                 <br />
                 <Typography component="span" variant="inherit" color="primary">
                   for Pneumonia Detection
@@ -73,7 +72,7 @@ const Hero = props => {
           alignItems="center"
           xs={12}
           md={6}
-          data-aos={'fade-up'}
+          data-aos={"fade-up"}
         >
           <Image
             src="https://i0.wp.com/bdtechtalks.com/wp-content/uploads/2020/04/artificial-intelligence-brain.jpg?w=1392&ssl=1"
